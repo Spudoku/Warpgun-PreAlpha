19b66106d963a11d5eae0bd7ec810c6a01ff0f33
Spudoku <calebisntshort@gmail.com>
2025-01-17 19:46:48 -0800

    60 minutes. Created a basic CharacterController script (based off of a YouTube video) and begun prototyping a "velocity-based" CharacterController for more subtle movement. Deleted Sample Scene. Created basic environment.

---
+++ Assets/Scripts/Movement/VelBasedController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+// "VelBased" means that movement is controlled by velocity.
+// Requires a RigidBody.
+public class VelBasedController : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float maxSpeed = 10f;               // normalize movement to this speed!
+    [SerializeField] float acceleration = 8f;           // acceleration in units/second/second
+
+    [SerializeField] float deecelleration = 8f;         // negative acceleration in units/second/second
+
+    float curSpeed = 0f;                                // magnitude of current movement
+    Vector3 moveDir;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        moveDir = new();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // calculate speed;
+        // take forward/backward input first
+        // begin building moveDir vector
+
+        Vector3 vertMove = new(0, rb.linearVelocity.y, 0);                              // take gravity into account for later
+        if (Input.GetAxis("Vertical") != 0 || Input.GetAxis("Horizontal") != 0)
+        {
+
+            moveDir = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized;
+            curSpeed += acceleration * Time.deltaTime;
+            // limit curSpeed
+            if (curSpeed > maxSpeed) curSpeed = maxSpeed;
+        }
+        else
+        {
+            curSpeed -= deecelleration * Time.deltaTime;
+        }
+        //Debug.Log("curSpeed: " + curSpeed);
+        // speed cannot go below 0 (SPEED, not VELOCITY)
+        if (curSpeed < 0f) curSpeed = 0f;
+
+
+        // set linear velocity to moveDir * curSpeed;
+        rb.linearVelocity = vertMove + (moveDir * curSpeed);
+        //Debug.Log("rb linearvelocity: " + rb.linearVelocity + ", vertMove" + vertMove + ", moveDir: " + moveDir + ", curSpeed: " + curSpeed);
+        Debug.Log("angular velocity: " + rb.angularVelocity);
+    }
+}
